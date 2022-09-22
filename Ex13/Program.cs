b44c34e800dd73a0806de1e7e7956c032f5192b8
Olga Lobanova <arapova@list.ru>
2022-09-22 18:20:59 +0300

    Задача 13

---
+++ Ex13/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите целое число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+if (a <0)
+{
+    a = -a;
+}
+if (a < 100)
+{
+    Console.WriteLine($"{a} -> Третьей цифры нет");
+}
+else
+{
+    int b = a;
+    while (a > 1000)
+    {
+        a = a / 10;
+    }
+    a = a % 10;
+    Console.WriteLine($"{b} -> {a}");
+}
